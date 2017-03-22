50c933bcf3230c62e756001242c968dcf6c2d599
miegli <michae.egli@phlu.ch>
2017-03-22 16:54:04 +0100

    removed onepage rendering on default page

--- Resources/Public/hybridsearch.js
+++ Resources/Public/hybridsearch.js
@@ -878,7 +878,7 @@
 
                                 hybridsearch.$$conf.branchisloading = true;
 
-                                $http.get((hybridsearch.$$conf.cdnDatabaseURL == undefined ? hybridsearch.$$conf.databaseURL : hybridsearch.$$conf.cdnDatabaseURL) + "/branches/" + hybridsearch.$$conf.workspace + ".json?shallow=true").success(function (data) {
+                                $http.get((hybridsearch.$$conf.cdnDatabaseURL == undefined ? hybridsearch.$$conf.databaseURL : hybridsearch.$$conf.cdnDatabaseURL) + "/branches/" + hybridsearch.$$conf.workspace + ".json").success(function (data) {
                                     hybridsearch.setBranch(data);
                                     isRunning = true;
                                 });
