98d7f8b9781da5945748f7f06414c0ef9c821719
miegli <michae.egli@phlu.ch>
2017-05-27 01:02:21 +0200

    bug fix

--- Classes/Neoslive/Hybridsearch/Factory/SearchIndexFactory.php
+++ Classes/Neoslive/Hybridsearch/Factory/SearchIndexFactory.php
@@ -1354,7 +1354,7 @@ class SearchIndexFactory
         $metaphone = mb_strtoupper(metaphone(mb_strtolower($string)));
 
         if (strlen($metaphone) > 5) {
-            $metaphone = mb_substr($metaphone,0,mb_strlen($metaphone)-2);
+            $metaphone = strlen($metaphone,0,strlen($metaphone)-2);
         }
 
 
