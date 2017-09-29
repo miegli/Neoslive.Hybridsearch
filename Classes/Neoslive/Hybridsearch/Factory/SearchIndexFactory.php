f481bc4950bd7ba445d4c4a0badad4dd8ac05e09
miegli <michae.egli@phlu.ch>
2017-09-29 13:55:26 +0200

    bug fix

--- Classes/Neoslive/Hybridsearch/Factory/SearchIndexFactory.php
+++ Classes/Neoslive/Hybridsearch/Factory/SearchIndexFactory.php
@@ -1397,7 +1397,12 @@ class SearchIndexFactory
                         $this->keywords->$workspaceHash->$dimensionConfigurationHash->$k = new \stdClass();
                     }
                     $this->keywords->$workspaceHash->$dimensionConfigurationHash->$k->$kek = $vev;
+
+                    
                     $k1 = mb_substr($k,0,4);
+                    if (property_exists($this->keywords->$workspaceHash->$dimensionConfigurationHash, $k1) === false) {
+                        $this->keywords->$workspaceHash->$dimensionConfigurationHash->$k1 = new \stdClass();
+                    }
                     $this->keywords->$workspaceHash->$dimensionConfigurationHash->$k1->$kek = $vev;
 
                 }
